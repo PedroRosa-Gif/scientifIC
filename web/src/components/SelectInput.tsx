16b77260f808e7e5abc274ac97027b05ffb5ba5f
PauloVictorSS <paulovictorsantos0@gmail.com>
2023-05-10 15:51:42 -0300

    Adicionando componentes e imagens que vão ser utlizadas na página

---
+++ web/src/components/SelectInput.tsx
@@ -0,0 +1,20 @@
+import { ReactNode, SelectHTMLAttributes } from "react";
+
+import "../styles/TextInput.css";
+
+interface ILogin extends SelectHTMLAttributes<HTMLSelectElement>{
+  icon: string;
+  children: ReactNode;
+  className?: string;
+}
+
+export default function SelectInput({ icon, children, className, ...props }:ILogin) {
+  return (
+    <div className="div-input-text">
+      <img src={icon} alt="Ícone de Login" />
+      <select className={"input-text " + className} { ...props } >
+        {children}
+      </select>
+    </div>
+  )
+}
