ea46d66f343bfdc2940acbae52ee065b8981d5ff
PauloVictorSS <paulovictorsantos0@gmail.com>
2023-05-10 15:52:12 -0300

    Criando a página de cadastro junto com a estilização dela

--- web/src/pages/SignUp.tsx
+++ web/src/pages/SignUp.tsx
@@ -1,5 +1,142 @@
-function SignUp() {
-    return (<>Sign UP</>)
-}
+import { useState } from "react";
 
-export default SignUp;
+import "../styles/Login.css";
+import "../styles/SignUp.css";
+
+import ContainerSign from "../components/ContainerSign";
+import TextInput from "../components/TextInput";
+import ButtonSign from "../components/ButtonSign";
+
+import UserIcon from "../assets/icons/user_icon.svg";
+import PassIcon from "../assets/icons/pass_icon.svg";
+import BackIcon from "../assets/icons/back_icon.svg";
+import SelectInput from "../components/SelectInput";
+import IUser from "../interfaces/IUser";
+import { createUser } from "../apis/user.endpoint";
+
+export default function SignUp() {
+  const [name, setName] = useState("");
+  const [lastName, setlastName] = useState("");
+  const [ra, setRA] = useState("");
+  const [birthdate, setBirthday] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [userType, setUserType] = useState(1);
+
+  const [currentPhase, setCurrentPhase] = useState(0);
+
+  async function handleCreateUser() {
+
+    if(password !== confirmPassword){
+      alert("Campo senha e confirmar senha estão diferentes");
+      return
+    }
+
+    const userInfos:IUser = {
+      email,
+      password,
+      name,
+      lastName,
+      ra,
+      birthdate: new Date(birthdate),
+      type: userType,
+    }
+    
+    const result = await createUser(userInfos);
+    console.log(result);
+  }
+
+  function previousPage(){
+    setCurrentPhase(0)
+  }
+  function nextPhase(){
+    setCurrentPhase(1)
+  }
+
+  return (
+    <ContainerSign>
+      <section className="login">
+        {currentPhase === 1 ?
+          <button className="button-back-page">
+            <img src={BackIcon} alt="Voltar"  onClick={previousPage}/>
+          </button>
+          :
+          <></>
+        }
+        <div className="header-login">
+          <span>Cadastrar</span>
+        </div>
+        {currentPhase === 0 ?
+          <div className="div-fields-login">
+            <TextInput
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              icon={UserIcon}
+              placeholder="Nome"
+              type="text"
+            />
+            <TextInput
+              value={lastName}
+              onChange={(e) => setlastName(e.target.value)}
+              icon={UserIcon}
+              placeholder="Sobrenome"
+              type="text"
+            />
+            <TextInput
+              value={ra}
+              onChange={(e) => setRA(e.target.value)}
+              icon={UserIcon}
+              placeholder="RA"
+              type="text"
+            />
+            <TextInput
+              value={birthdate}
+              onChange={(e) => setBirthday(e.target.value)}
+              icon={UserIcon}
+              placeholder="Nome"
+              type="date"
+            />
+            <div className="div-button-login">
+              <ButtonSign title="Próximo" onClick={nextPhase}/>
+            </div>
+          </div>
+          :
+          <div className="div-fields-login">
+            <TextInput
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              icon={UserIcon}
+              placeholder="Email"
+              type="text"
+            />
+            <TextInput
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              icon={PassIcon}
+              placeholder="Senha"
+              type="text"
+            />
+            <TextInput
+              value={confirmPassword}
+              onChange={(e) => setConfirmPassword(e.target.value)}
+              icon={PassIcon}
+              placeholder="Confirme sua senha"
+              type="text"
+            />
+            <SelectInput icon={PassIcon} onChange={(e) => setUserType(parseInt(e.target.value))}>
+              <option value="1">Estudante</option>
+              <option value="2">Professor</option>
+            </SelectInput>
+            <div className="div-button-login">
+              <ButtonSign title="Cadastrar" onClick={() => handleCreateUser()}/>
+            </div>
+          </div>
+        }
+        <div className="div-create-account">
+          <span>Já possui conta? <b>Faça login</b></span>
+        </div>
+      </section>
+    </ContainerSign>
+  );
+}
