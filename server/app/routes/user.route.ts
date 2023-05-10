a4237699fa4c6855ddd4e91d559593d0745b5d27
PauloVictorSS <paulovictorsantos0@gmail.com>
2023-05-10 15:45:09 -0300

    Deletando alguns arquivos não usados mais

--- server/app/routes/user.route.ts
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { create } from "../controllers/user.controller";
-import { resolver } from "../adapters/route.adapter";
-
-const userRoutes = express.Router();
-
-userRoutes.post("/create", resolver(create));
-
-export default userRoutes;
